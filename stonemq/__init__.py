3ee70e441c3d0297b0ccfbce80a8fb2003c59794
mralves <mateusra@id.uff.br>
2015-11-05 18:38:23 -0200

    Initial Commit

---
+++ stonemq/__init__.py
@@ -0,0 +1,22 @@
+'''
+stonemq: Main module
+
+Copyright 2015, mralves
+Licensed under MIT.
+'''
+
+
+def main():
+    '''
+    Main function of the boilerplate code is the entry point of the 'stonemq' executable script (defined in setup.py).
+    
+    Use doctests, those are very helpful.
+    
+    >>> main()
+    Hello
+    >>> 2 + 2
+    4
+    '''
+    
+    print("Hello")
+
---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+'''
+stonemq: tests module.
+
+Meant for use with py.test.
+Organize tests into files, each named xxx_test.py
+Read more here: http://pytest.org/
+
+Copyright 2015, mralves
+Licensed under MIT
+'''
